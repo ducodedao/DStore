edd66b61e41b3f4afd0864ba53cae0970774565b
ducodedao <tongodu95@gmail.com>
2022-07-18 16:12:54 +0700

    Server Connect MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 const app = require('./app')
 const env = require('dotenv')
+const connectDatabase = require('./conf/mongodb')
 
 // Config
 env.config()
+
+// Connect MongoDB
+connectDatabase()
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}.`)
