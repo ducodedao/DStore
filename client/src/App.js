10b7158850b0e9e53d65c94ba296fe2788be9a5f
ducodedao <tongodu95@gmail.com>
2022-07-18 15:52:21 +0700

    Client Init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import './App.scss'
+
+const App = () => {
+    return (
+        <Fragment>
+            <div>DStore</div>
+        </Fragment>
+    )
+}
+
+export default App
