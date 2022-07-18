3fbddbb60042c7c0fe3aabea89131f3226723ca3
ducodedao <tongodu95@gmail.com>
2022-07-18 16:22:06 +0700

    Server Error Handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const errMiddleware = require('./middlewares/errMiddleware')
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Middleware for Errors
+app.use(errMiddleware)
 
 module.exports = app
---
+++ server/src/middlewares/errMiddleware.js
@@ -0,0 +1,35 @@
+const ErrorHander = require('../utils/ErrorHander')
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || 'Internal Server Error.'
+
+    // Wrong Mongodb Id Error
+    if (err.name === 'CastError') {
+        const message = `Resource Not Found. Invalid: ${err.path}.`
+        err = new ErrorHander(message, 400)
+    }
+
+    // Mongoose Duplicate Key Error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered.`
+        err = new ErrorHander(message, 400)
+    }
+
+    // Wrong JWT Error
+    if (err.name === 'JsonWebTokenError') {
+        const message = `Json Web Token Is Invalid, Try Again.`
+        err = new ErrorHander(message, 400)
+    }
+
+    // JWT Expire Error
+    if (err.name === 'TokenExpiredError') {
+        const message = `Json Web Token Is Expired, Try Again.`
+        err = new ErrorHander(message, 400)
+    }
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    })
+}
