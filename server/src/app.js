a7ef501aa1aa7c75f452e6c4008156a84193d60a
ducodedao <tongodu95@gmail.com>
2022-07-21 20:16:46 +0700

    Server User API

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const errMiddleware = require('./middlewares/errMiddleware')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload())
+
+// Route
+const user = require('./routes/userRoute')
+
+app.use('/api/v1', user)
 
 // Middleware for Errors
 app.use(errMiddleware)
