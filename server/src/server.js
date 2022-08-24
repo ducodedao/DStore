db4db0c5812781aeea650184477808826d54557c
ducodedao <tongodu95@gmail.com>
2022-08-24 15:15:48 +0700

    Server Error Handling

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,14 @@ const app = require('./app')
 const env = require('dotenv')
 const connectMongoDB = require('./config/mongoDB')
 
+// Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+	console.log(`Error: ${err.message}.`)
+	console.log(`Shutting down the server due to Uncaught Exception.`)
+
+	process.exit(1)
+})
+
 // Config
 env.config()
 
@@ -10,4 +18,14 @@ connectMongoDB()
 
 const server = app.listen(process.env.PORT, () => {
 	console.log(`Server is running on http://localhost:${process.env.PORT}.`)
+})
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+	console.log(`Error: ${err.message}.`)
+	console.log(`Shutting down the server due to Unhandled Promise Rejection.`)
+
+	server.close(() => {
+		process.exit(1)
+	})
 })
